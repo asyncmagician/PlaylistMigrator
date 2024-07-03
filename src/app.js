046dd47554ab602c57d8c75a22c858174fa5d5c9
Antony BARTOLOMUCCI <giammarino.antony@gmail.com>
2024-07-03 13:06:36 +0200

    Support HTTPS + add CI + update .gitignore

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
+import https from 'https';
+import fs from 'fs';
 import express from 'express';
 import open from 'open';
-import config from './config/config.js';
-import { authenticateSpotify } from './auth/spotify/auth.js';
+import dotenv from 'dotenv';
+import { authenticateSpotify } from './src/auth/spotify/auth.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = 8888;
 
+const options = {
+    key: fs.readFileSync('certs/server.key'),
+    cert: fs.readFileSync('certs/server.cert')
+};
+
 app.get('/callback', authenticateSpotify);
 
-app.listen(PORT, () => {
-    const authorizeURL = `https://accounts.spotify.com/authorize?client_id=${config.spotifyClientId}&response_type=code&redirect_uri=${encodeURIComponent(config.spotifyRedirectUri)}&scope=playlist-modify-public%20playlist-modify-private`;
+https.createServer(options, app).listen(PORT, () => {
+    const authorizeURL = `https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(process.env.SPOTIFY_REDIRECT_URI)}&scope=playlist-modify-public%20playlist-modify-private`;
     console.log(`Listening on port ${PORT}. Please go to the following URL to authenticate: ${authorizeURL}`);
     open(authorizeURL);
 });
-
-export default app;
